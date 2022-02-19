0993aaa8323d8818978b42de2cf107c91dcdbc93
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-02-19 15:57:29 +0530

    [neeta mourya] check employee is present or absent

---
+++ src/main/java/com/bridgelabz/employeewagecomputation/EmpWageBuilderUC1.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.employeewagecomputation;
+
+public class EmpWageBuilderUC1 {
+
+	public static void main(String[] args) {
+		// Contants
+		int IS_FULL_TIME = 1;
+		// Computations
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
+
+	}
+}
