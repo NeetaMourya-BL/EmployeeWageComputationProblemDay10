bdd59cdc5df023926d8d7d9a278082fd78089d33
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-02-19 15:53:51 +0530

    [neeta mourya] display welcome message in master branch

---
+++ src/main/java/com/bridgelabz/employeewagecomputation/EmpWageBuilder.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.employeewagecomputation;
+
+/**
+ * Employee Wage Computation Problem!
+ *
+ */
+public class EmpWageBuilder {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Employee Wage Computation Program");
+		
+	}
+}
